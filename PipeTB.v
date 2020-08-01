e574b6470a3f38da8296da12bef7a5c1417c5aa8
sss <sssanikhani@gmail.com>
2020-08-01 18:34:02 +0430

    Debugging

--- PipeTB.v
+++ PipeTB.v
@@ -4,7 +4,7 @@ module PipeTB();
     reg clk, rst;
 
     PipeLine MipsPipe(clk, rst);
-    
+    // 1st Test: MEM2000 ---> 00000000000000011110100001111100  : 125052
     
     initial begin
         #10 rst = 0; clk = 0; #10 rst = 1; #10 rst = 0;
