37c391f307f1e31066929901d167ef4b21924b06
sss <sssanikhani@gmail.com>
2020-08-04 01:05:33 +0430

    TB updated

--- PipeTB.v
+++ PipeTB.v
@@ -6,6 +6,9 @@ module PipeTB();
     PipeLine MipsPipe(clk, rst);
     // 1st Test: MEM2000 ---> 00000000000000011110100001111100  : 125052
     
+    // 2nd Test: MEM2004 ---> 00000000000000000000000000010001  : 17
+    //           MEM2000 ---> 00000000000011110000000000001000  : 983048
+
     initial begin
         #10 rst = 0; clk = 0; #10 rst = 1; #10 rst = 0;
         repeat (1000) begin
